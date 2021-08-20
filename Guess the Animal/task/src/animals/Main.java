cd39f3d1ab79c8c0df5e530f49d3e1fb2699cebc
Andrey-Metelyov <andrey.metelyov@gmail.com>
2021-08-20 19:51:44 +0900

    Stage 2/6: Animal facts in process

--- Guess the Animal/task/src/animals/Main.java
+++ Guess the Animal/task/src/animals/Main.java
@@ -1,17 +1,57 @@
 package animals;
 
 import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Main {
     static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
         greetings();
-        String animal = enterAnimal();
-        System.out.println("Is it " + animal + "?");
+        String animal1 = enterAnimal("Enter the first animal:");
+        String animal2 = enterAnimal("Enter the second animal:");
+        String fact = enterFact(animal1, animal2);
+//        System.out.println("Is it " + animal + "?");
         String answer = getAnswer();
         System.out.println("You answered: " + answer);
         bye();
+    }
+
+    private static String enterFact(String animal1, String animal2) {
+        String result = "";
+        while (true) {
+            System.out.println("Specify a fact that distinguishes " +
+                    animal1 + " from " + animal2 + ".\n" +
+                    "The sentence should be of the format: 'It can/has/is ...'.");
+            String input = scanner.nextLine().trim().toLowerCase();
+            Pattern pattern = Pattern.compile("^it\\s+(can|has|is)\\s+(.+)");
+            Matcher matcher = pattern.matcher(input);
+            if (matcher.matches()) {
+                String verb = matcher.group(1);
+                String fact = addArticle(matcher.group(2));
+                System.out.println("Is it correct for " + animal2 + "?");
+                String answer = getAnswer();
+                System.out.println("I learned the following facts about animals:");
+                if (answer.equals("Yes")) {
+                    System.out.println(" - The " + animal1 + " " + verb + "n't " + fact);
+                    System.out.println(" - The " + animal2 + " " + verb + " " + fact);
+                } else {
+                    System.out.println(" - The " + animal1 + " " + verb + " " + fact);
+                    System.out.println(" - The " + animal2 + " " + verb + "n't " + fact);
+                }
+                System.out.println("I can distinguish these animals by asking the question:");
+                result = verb + " it " + fact + "?";
+                System.out.println(result);
+                break;
+            } else {
+                System.out.println("The examples of a statement:\n" +
+                        " - It can fly\n" +
+                        " - It has horn\n" +
+                        " - It is a mammal");
+            }
+        }
+        return result;
     }
 
     private static void bye() {
@@ -69,8 +109,8 @@ public class Main {
         return article + " " + word;
     }
 
-    private static String enterAnimal() {
-        System.out.println("Enter an animal:");
+    private static String enterAnimal(String prompt) {
+        System.out.println(prompt);
         String[] input = scanner.nextLine().toLowerCase().split(" ", 2);
         String animal;
         if (input.length > 1) {
